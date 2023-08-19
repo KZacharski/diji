5f5b63b6f7fdadabd32fe4f5e8d74eb4e822fb8b
KZacharski <puffercatt@gmail.com>
2023-08-19 21:30:03 +0200

    started working on the menu

--- docs/docs/script.js
+++ docs/docs/script.js
@@ -1,8 +1,17 @@
+var isOpen = false
+
 function openNav() {
     document.getElementById("drawer").style.width = "250px";
+    document.getElementById("drawerbg").style.width = "100%";
+
+    isOpen = true
   }
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("drawer").style.width = "0";
+    document.getElementById("drawerbg").style.width = "0";
+    isOpen = false
   } 
+
+  
